aba7d5a96500d47394c3f4cd5523b0fb57945f08
leejunho0622 <lee49409780@gmail.com>
2025-02-20 12:43:14 +0900

    Update files

---
+++ src/main/java/com/drscan/web/users/util/RedisService.java
@@ -0,0 +1,26 @@
+package com.drscan.web.users.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final StringRedisTemplate redisTemplate;
+
+    public void saveToken(String email, String token, long expiration) {
+        redisTemplate.opsForValue().set(email, token, expiration, TimeUnit.MINUTES);
+    }
+
+    public String getToken(String email) {
+        return redisTemplate.opsForValue().get(email);
+    }
+
+    public void deleteToken(String email) {
+        redisTemplate.delete(email);
+    }
+}
+
