2cc500a749b2e5c31f1b9187802f59d9708ceecc
leejunho0622 <lee49409780@gmail.com>
2025-02-21 17:05:35 +0900

    Add create user method

--- src/main/java/com/drscan/web/primary/users/service/UserService.java
+++ src/main/java/com/drscan/web/primary/users/service/UserService.java
@@ -17,4 +17,21 @@ public class UserService {
     public User findUserByUsername(String username){
         return userRepository.findUserByUsername(username);
     }
+
+    public String createUser(User user) {
+        User existUserByUsername = userRepository.findUserByUsername(user.getUsername());
+        User existUserByEmail = userRepository.findUserByEmail(user.getEmail());
+        User existUserByPhone = userRepository.findUserByPhone(user.getPhone());
+
+        if(existUserByUsername != null){
+            return "아이디가 중복됩니다.";
+        } else if(existUserByEmail != null){
+            return "이메일이 중복됩니다.";
+        } else if(existUserByPhone != null){
+            return "전화번호가 중복됩니다.";
+        }
+
+        userRepository.save(user);
+        return "success";
+    }
 }
