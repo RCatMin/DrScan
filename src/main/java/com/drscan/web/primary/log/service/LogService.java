0f4396a2c378ca564631739b26bb616de09d72eb
zkdn46 <123gusckd123@naver.com>
2025-02-21 17:22:19 +0900

    Add createLog()

--- src/main/java/com/drscan/web/primary/log/service/LogService.java
+++ src/main/java/com/drscan/web/primary/log/service/LogService.java
@@ -1,6 +1,8 @@
 package com.drscan.web.primary.log.service;
 
+import com.drscan.web.primary.log.domain.Log;
 import com.drscan.web.primary.log.domain.LogRepository;
+import com.drscan.web.primary.log.domain.LogRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,13 @@ public class LogService {
 
     private final LogRepository logRepository;
 
+    public Log createLog(LogRequestDto logDto) {
+        Log newLog = new Log(
+                logDto.getUserCode(),
+                logDto.getReportCode(),
+                logDto.getEvent()
+        );
+        return logRepository.save(newLog);
+    }
 
 }
