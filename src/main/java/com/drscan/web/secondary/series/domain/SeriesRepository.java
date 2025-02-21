eb0ed6572242b72782faa96f81c7d92603fa184b
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-21 17:56:28 +0900

    Modify SeriesRepository, SeriesService

---
+++ src/main/java/com/drscan/web/secondary/series/domain/SeriesRepository.java
@@ -0,0 +1,12 @@
+package com.drscan.web.secondary.series.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SeriesRepository extends JpaRepository<Series, SeriesId> {
+
+    public List<Series> findSriesByStudykey(Integer studykey);
+}
