4e2628b5ce74a78df24f918ce807470cf90518a6
leejunho0622 <lee49409780@gmail.com>
2025-02-21 14:57:42 +0900

    Update singin repsonse

--- src/main/java/com/drscan/web/Controller/UsersRestController.java
+++ src/main/java/com/drscan/web/Controller/UsersRestController.java
@@ -1,13 +1,13 @@
 package com.drscan.web.Controller;
 
 import com.drscan.web.primary.users.domain.User;
+import com.drscan.web.primary.users.domain.UserRequestDto;
 import com.drscan.web.primary.users.service.UserService;
+import com.drscan.web.primary.users.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,7 +29,14 @@ public class UsersRestController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> signin(){
-        return ResponseEntity.ok("");
+    public ResponseEntity<ResponseDto> signin(@RequestBody UserRequestDto userRequestDto) {
+        User user = userService.findUserByUsername(userRequestDto.getUsername());
+
+        if(!userRequestDto.getPassword().equals(user.getPassword())){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
+                    .body(new ResponseDto(HttpStatus.NOT_FOUND.value(), "아이디 혹은 비밀번호가 일치하지 않습니다."));
+        }
+
+        return ResponseEntity.ok(new ResponseDto(HttpStatus.OK.value(), "로그인에 성공했습니다."));
     }
 }
