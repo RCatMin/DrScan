56a41d430fd102f0a17f205a92781bf21a066077
leejunho0622 <lee49409780@gmail.com>
2025-02-21 14:59:38 +0900

    Add column annotation

--- src/main/java/com/drscan/web/primary/users/domain/User.java
+++ src/main/java/com/drscan/web/primary/users/domain/User.java
@@ -1,9 +1,6 @@
 package com.drscan.web.primary.users.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.sql.Timestamp;
@@ -13,19 +10,34 @@ import java.sql.Timestamp;
 @Table(name = "users", schema = "drscan_db", uniqueConstraints = @UniqueConstraint(columnNames = {"username", "email", "phone", "otp_key"}))
 public class User {
     @Id
+    @Column(name = "user_code")
     private Integer userCode;
 
+    @Column(name = "account_type")
     private String accountType;
+
     private String username;
     private String password;
+
+    @Column(name = "hospital_name")
     private String hospitalName;
+
     private String department;
     private String name;
     private String email;
     private String phone;
+
+    @Column(name = "otp_key")
     private String otpKey;
+
     private String status;
+
+    @Column(name = "fail_count")
     private Integer failCount;
+
+    @Column(name = "reg_date")
     private Timestamp regDate;
+
+    @Column(name = "mod_date")
     private Timestamp modDate;
 }
