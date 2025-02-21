d4153e26443c19308ca30e683d5c8e0c3dd47d4c
leejunho0622 <lee49409780@gmail.com>
2025-02-21 17:08:29 +0900

    Update signup method and Add login session

--- src/main/java/com/drscan/web/Controller/UsersRestController.java
+++ src/main/java/com/drscan/web/Controller/UsersRestController.java
@@ -2,8 +2,11 @@ package com.drscan.web.Controller;
 
 import com.drscan.web.primary.users.domain.User;
 import com.drscan.web.primary.users.domain.UserRequestDto;
+import com.drscan.web.primary.users.service.TokenService;
 import com.drscan.web.primary.users.service.UserService;
 import com.drscan.web.primary.users.util.ResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,18 +27,41 @@ public class UsersRestController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(){
-        return ResponseEntity.ok("");
+    public ResponseEntity<ResponseDto> signup(@RequestBody UserRequestDto userDto) {
+        String username = userDto.getUsername();
+        String password = userDto.getPassword();
+        String hospital = userDto.getHospital();
+        String department = userDto.getDepartment();
+        String name = userDto.getName();
+        String email = userDto.getEmail();
+        String phone = userDto.getPhone();
+
+        String otpKey = TokenService.generateSecretKey();
+
+        User user = new User(username, password, hospital, department, name, email, phone, otpKey);
+
+        String isSuccess = userService.createUser(user);
+
+        if(!isSuccess.equals("success")){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST.value())
+                    .body(new ResponseDto(HttpStatus.BAD_REQUEST.value(), isSuccess));
+        }
+
+        return ResponseEntity.ok(new ResponseDto(HttpStatus.OK.value(), "회원가입 요청이 완료되었습니다."));
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<ResponseDto> signin(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<ResponseDto> signin(@RequestBody UserRequestDto userRequestDto, HttpServletRequest request) {
         User user = userService.findUserByUsername(userRequestDto.getUsername());
 
         if(!userRequestDto.getPassword().equals(user.getPassword())){
             return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
                     .body(new ResponseDto(HttpStatus.NOT_FOUND.value(), "아이디 혹은 비밀번호가 일치하지 않습니다."));
         }
+
+        HttpSession session = request.getSession();
+        session.setAttribute("authUser", user);
 
         return ResponseEntity.ok(new ResponseDto(HttpStatus.OK.value(), "로그인에 성공했습니다."));
     }
