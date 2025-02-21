52ca8b63bd8f6699f4a11fab3ae86c0ce052d1f8
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-21 16:21:26 +0900

    Add PatientScanRESTController.java

---
+++ src/main/java/com/drscan/web/Controller/PatientScanRESTController.java
@@ -0,0 +1,22 @@
+package com.drscan.web.Controller;
+
+import com.drscan.web.patientScan.service.PatientScanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/patient/action")
+@RestController
+public class PatientScanRESTController {
+
+    private final PatientScanService patientScanService;
+
+    @GetMapping("/{pid}")
+    public ResponseEntity<?> getPatientStudies(@PathVariable String pid) {
+        return ResponseEntity.ok(patientScanService.getStudiesByPatientId(pid));
+    }
+}
