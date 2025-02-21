a3c45a82a2edc731c40a2e86238d6bce7ae5c440
leejunho0622 <lee49409780@gmail.com>
2025-02-21 14:56:28 +0900

    Add request and response UserDto

---
+++ src/main/java/com/drscan/web/primary/users/domain/UserRequestDto.java
@@ -0,0 +1,15 @@
+package com.drscan.web.primary.users.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/drscan/web/primary/users/util/ResponseDto.java
@@ -0,0 +1,17 @@
+package com.drscan.web.primary.users.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+
+    private int statusCode;
+    private String message;
+
+}
