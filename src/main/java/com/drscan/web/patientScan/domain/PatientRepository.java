15ff1fc5b68f2fdee6bfcea32eaf4f40df37354b
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-21 16:21:49 +0900

    Modify PatientRepository.java

--- src/main/java/com/drscan/web/patientScan/domain/PatientRepository.java
+++ src/main/java/com/drscan/web/patientScan/domain/PatientRepository.java
@@ -3,6 +3,11 @@ package com.drscan.web.patientScan.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+    List<Patient> findByPid(String pid);
+
 }
