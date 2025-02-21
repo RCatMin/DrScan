020654efcae5f2213085ca6b5a7cfa46d8d11518
leejunho0622 <lee49409780@gmail.com>
2025-02-21 14:59:05 +0900

    Modify file path

---
+++ src/main/java/com/drscan/web/primary/users/service/TokenService.java
@@ -0,0 +1,57 @@
+package com.drscan.web.primary.users.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+@Service
+public class TokenService {
+
+    private final String SECRET_KEY;
+
+    public TokenService() {
+        byte[] key = new byte[32];
+        new SecureRandom().nextBytes(key);
+        this.SECRET_KEY = Base64.getEncoder().encodeToString(key);
+    }
+
+    public String generateToken(String email) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))  // 1시간 만료
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+}
