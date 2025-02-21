e495f24b7d162d64a7c4356321270a7a404c8d50
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-21 12:04:11 +0900

    Rename PatientScanRestController -> PatientScanController

--- src/main/java/com/drscan/web/Controller/PatientScanController.java
+++ src/main/java/com/drscan/web/Controller/PatientScanController.java
@@ -1,14 +1,12 @@
 package com.drscan.web.Controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RequestMapping("/patientScan")
-@RestController
-public class PatientScanRestController {
+@Controller
+public class PatientScanController {
 
     @GetMapping("/search")
     public String search() { return "patient-imaging-record/patient-search"; }
