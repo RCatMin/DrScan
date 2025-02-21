0e71d3c791496e3ff1c565b79b405f24d8da94c7
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-21 17:56:40 +0900

    Modify StudyRepository

--- src/main/java/com/drscan/web/secondary/study/domain/StudyRepository.java
+++ src/main/java/com/drscan/web/secondary/study/domain/StudyRepository.java
@@ -3,7 +3,9 @@ package com.drscan.web.secondary.study.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Integer> {
-
+    List<Study> findByPid(String pid);
 }
