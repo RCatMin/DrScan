76b6ec2462518b042b6dec8a52e51063d32a6856
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-23 15:32:58 +0900

    Modify PatientScanRESTController

--- src/main/java/com/drscan/web/Controller/PatientScanRESTController.java
+++ src/main/java/com/drscan/web/Controller/PatientScanRESTController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/patient/action")
+@RequestMapping("/patientScan/action")
 @RestController
 public class PatientScanRESTController {
 
