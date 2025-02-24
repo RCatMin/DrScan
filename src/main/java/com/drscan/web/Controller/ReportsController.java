53e5d7bb9891fec748e204d14f6dcd322e6a7ede
RCatMin <razzcat_min@naver.com>
2025-02-24 15:47:54 +0900

    Modify ReportsController

--- src/main/java/com/drscan/web/Controller/ReportsController.java
+++ src/main/java/com/drscan/web/Controller/ReportsController.java
@@ -1,22 +1,44 @@
 package com.drscan.web.Controller;
 
-import com.drscan.web.primary.reports.service.ReportsService;
-
+import com.drscan.web.secondary.patientScan.service.PatientScanService;
+import com.drscan.web.secondary.series.domain.Series;
+import com.drscan.web.secondary.series.service.SeriesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/reports")
 @Controller
 public class ReportsController {
 
-    private final ReportsService reportsService;
+    private final PatientScanService patientScanService;
+    private final SeriesService seriesService;
 
-    @GetMapping("/views/{patientId}")
-    public ResponseEntity<?> findPatientAll(){
-        return ResponseEntity.ok("");
+    @GetMapping("/views/{patientId}/{studyKey}")
+    public ResponseEntity<?> getPatientRecords(@PathVariable String patientId) {
+        Map<String, Object> patientResponse = patientScanService.getPatientRecords(patientId);
+
+        if (patientResponse.containsKey("error")){
+            return ResponseEntity.badRequest().body(patientResponse);
+        }
+        return ResponseEntity.ok(patientResponse);
     }
+
+    public ResponseEntity<?> getStudyRecords(@PathVariable Integer studyKey) {
+        List<Series> seriesResponse = seriesService.findSeriesAll(studyKey);
+
+        if (seriesResponse.isEmpty()){
+            return ResponseEntity.badRequest().body(seriesResponse);
+        }
+
+        return ResponseEntity.ok(seriesResponse);
+    }
+
 }
