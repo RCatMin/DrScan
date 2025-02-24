93215a161b0c1199c774223055d2fb982f3b5acc
leejunho0622 <lee49409780@gmail.com>
2025-02-24 18:09:51 +0900

    Code Refactor

--- src/main/java/com/drscan/web/primary/users/domain/UserRepository.java
+++ src/main/java/com/drscan/web/primary/users/domain/UserRepository.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findUserByUsername(String username);
-    public User findUserByEmail(String email);
-    public User findUserByPhone(String phone);
+    User findUserByUsername(String username);
+    User findUserByEmail(String email);
+    User findUserByPhone(String phone);
 
-    public List<User> findAllByAccountType(String accountType);
-    public List<User> findAllByAccountTypeAndStatus(String accountType, String status);
+    List<User> findAllByAccountType(String accountType);
+    List<User> findAllByAccountTypeAndStatus(String accountType, String status);
 }
--- src/main/webapp/WEB-INF/views/users/signin.jsp
+++ src/main/webapp/WEB-INF/views/users/signin.jsp
@@ -5,7 +5,7 @@
   Time: 오전 10:28
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8"%>
 <%@ taglib prefix="c" uri="jakarta.tags.core"%>
 <html>
 <head>
