ea207bfea3bae82aeae109ac32ea37b22cbc68d0
leejunho0622 <lee49409780@gmail.com>
2025-02-24 17:38:11 +0900

    Add findAllByAccountType and findAllByAccountTypeAndStatus methods

--- src/main/java/com/drscan/web/primary/users/domain/UserRepository.java
+++ src/main/java/com/drscan/web/primary/users/domain/UserRepository.java
@@ -3,9 +3,14 @@ package com.drscan.web.primary.users.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findUserByUsername(String username);
     public User findUserByEmail(String email);
     public User findUserByPhone(String phone);
+
+    public List<User> findAllByAccountType(String accountType);
+    public List<User> findAllByAccountTypeAndStatus(String accountType, String status);
 }
