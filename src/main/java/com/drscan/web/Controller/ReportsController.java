3062c37fda138137c7dd502ec8beb8ddd4e6ee80
RCatMin <razzcat_min@naver.com>
2025-02-26 18:00:08 +0900

    Endpoints, parameters, and return types have been changed.

--- src/main/java/com/drscan/web/Controller/ReportsController.java
+++ src/main/java/com/drscan/web/Controller/ReportsController.java
@@ -1,5 +1,6 @@
 package com.drscan.web.Controller;
 
+import com.drscan.web.secondary.image.domain.Image;
 import com.drscan.web.secondary.patientScan.service.PatientScanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 
-import java.util.Map;
+import java.util.List;
 
 @RequestMapping("/reports")
 @Controller
@@ -18,18 +19,12 @@ public class ReportsController {
 
     private final PatientScanService patientScanService;
 
-    @GetMapping("/views/test/{pId}")
-    public String viewPatientReport(@PathVariable String pId, Model model) {
-        Map<String, Object> patientReport = patientScanService.getPatientRecords(pId);
-
-        if (patientReport.containsKey("error")) {
-            model.addAttribute("error", patientReport.get("error"));
-            return "error";  // 오류 페이지로 이동
-        }
-
-        System.out.println("환자 정보 : " + patientReport);
+    @GetMapping("/views/{studykey}/{serieskey}")
+    public String reportsPatient(@PathVariable Integer studykey, @PathVariable Integer serieskey, Model model) {
+        List<Image> patientReport = patientScanService.getImagesByStudyAndSeries(studykey, serieskey);
         model.addAttribute("patientReport", patientReport);
-        return "reports/reports";
+
+        return "reports/viewer";
     }
 
 }
