3f59420644d3844ee85b88945979484824447372
gusrbs-0404 <gusrbs7534@gmail.com>
2025-02-26 15:45:40 +0900

    Modify PatientScanService

--- src/main/java/com/drscan/web/secondary/patientScan/domain/PatientScanRepository.java
+++ src/main/java/com/drscan/web/secondary/patientScan/domain/PatientScanRepository.java
@@ -3,13 +3,9 @@ package com.drscan.web.secondary.patientScan.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
-public interface PatientScanRepository extends JpaRepository<Patient, Integer> {
-
-    //List<Patient> findByPid(String pid);
-
-    Optional<Patient> findByPid(String pid);
-    // 다시확인하기
+public interface PatientScanRepository extends JpaRepository<Patient, String> {
+    List<Patient> findByPid(String pid);
 }
--- src/main/java/com/drscan/web/secondary/patientScan/service/PatientScanService.java
+++ src/main/java/com/drscan/web/secondary/patientScan/service/PatientScanService.java
@@ -1,12 +1,16 @@
 package com.drscan.web.secondary.patientScan.service;
 
 import com.drscan.web.secondary.image.domain.Image;
+
 import com.drscan.web.secondary.image.domain.ImageRepository;
 import com.drscan.web.secondary.patientScan.domain.Patient;
+
 import com.drscan.web.secondary.patientScan.domain.PatientScanRepository;
 import com.drscan.web.secondary.series.domain.Series;
+
 import com.drscan.web.secondary.series.domain.SeriesRepository;
 import com.drscan.web.secondary.study.domain.Study;
+
 import com.drscan.web.secondary.study.domain.StudyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,8 +30,7 @@ public class PatientScanService {
         Map<String, Object> result = new HashMap<>();
 
         // 환자 정보 조회
-        Optional<Patient> patient = patientRepository.findByPid(pid);
-        // 다시확인하기 @@@@@@@
+        Optional<Patient> patient = patientRepository.findById(pid);
         if (patient.isEmpty()) {
             result.put("error", "환자 정보 없음!");
             return result;
