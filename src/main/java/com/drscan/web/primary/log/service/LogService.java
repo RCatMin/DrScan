927ecc0f8ed376c76608725ad79868ffbeedfec5
zkdn46 <123gusckd123@naver.com>
2025-02-28 17:23:12 +0900

    Fix Log

--- src/main/java/com/drscan/web/primary/log/service/LogService.java
+++ src/main/java/com/drscan/web/primary/log/service/LogService.java
@@ -2,8 +2,6 @@ package com.drscan.web.primary.log.service;
 
 import com.drscan.web.primary.log.domain.Log;
 import com.drscan.web.primary.log.domain.LogRepository;
-import com.drscan.web.primary.log.domain.LogRequestDto;
-import com.drscan.web.primary.reports.domain.SeriesList;
 import com.drscan.web.primary.users.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,24 +12,10 @@ public class LogService {
 
     private final LogRepository logRepository;
 
-    public Log createLog(LogRequestDto logDto) {
-        Log newLog = new Log(
-                logDto.getUserCode(),
-                logDto.getReportCode(),
-                logDto.getEvent()
-        );
-        return logRepository.save(newLog);
-    }
-
     public void saveLog(User user, String event) {
         Log log = new Log(user.getUserCode(), event);
 
         logRepository.save(log);
     }
 
-    public void saveLog(User user, SeriesList seriesList, String event) {
-        Log log = new Log(user.getUserCode(), seriesList.getSeriesKey(), event);
-
-        logRepository.save(log);
-    }
 }
