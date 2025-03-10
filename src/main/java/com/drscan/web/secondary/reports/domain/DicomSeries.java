bfeb65c516bc6b6efc16833039cbdf33473febb8
RCatMin <razzcat_min@naver.com>
2025-03-10 17:19:27 +0900

    Add DicomSeries Entity

---
+++ src/main/java/com/drscan/web/secondary/reports/domain/DicomSeries.java
@@ -0,0 +1,41 @@
+package com.drscan.web.secondary.reports.domain;
+
+import com.drscan.web.secondary.series.domain.SeriesId;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Entity
+@Table(name = "seriestab", schema = "pacsplus")
+@IdClass(SeriesId.class)
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class DicomSeries {
+
+    @Id
+    private Integer studykey;
+
+    @Id
+    private Integer serieskey;
+
+    private String studyinsuid;
+
+    @Column(unique=true)
+    private String seriesinsuid;
+    private String seriesnum;
+    private String modality;
+    private String seriesdate;
+    private String seriestime;
+    private String bodypart;
+    private String seriesdesc;
+    private Integer imagecnt;
+    private Integer delflag;
+
+    public DicomSeries(Integer serieskey, String modality) {
+        this.serieskey = serieskey;
+        this.modality = modality;
+    }
+}
+
