06bdd5c251e9911a775d185f45f3550c5df77342
zkdn46 <123gusckd123@naver.com>
2025-03-10 17:54:02 +0900

    Update text

--- src/main/resources/static/script/signin.js
+++ src/main/resources/static/script/signin.js
@@ -168,7 +168,7 @@ async function loginAction(username, password, code) {
         return true;
     } else {
         const json = await response.json();
-        alert(`오류 ${json.message}`);
+        alert(`${json.message}`);
         return json.isValid;
     }
 }
