5c4902bcfcf4e18dca56aaf270cfcc22392e4a3a
RCatMin <razzcat_min@naver.com>
2025-03-10 17:22:20 +0900

    "dcm4che 적용을 위해 Controller의 Get 요청을 수정함"

--- src/main/java/com/drscan/web/Controller/ReportsController.java
+++ src/main/java/com/drscan/web/Controller/ReportsController.java
@@ -1,16 +1,12 @@
 package com.drscan.web.Controller;
 
-import com.drscan.web.secondary.image.domain.Image;
 import com.drscan.web.secondary.patientScan.service.PatientScanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 
-
-import java.util.List;
 
 @RequestMapping("/reports")
 @Controller
@@ -19,8 +15,8 @@ public class ReportsController {
 
     private final PatientScanService patientScanService;
 
-    @GetMapping("/views/{pid}/{studykey}/{serieskey}")
-    public String reportsPatient(@PathVariable String pid, @PathVariable Integer studykey, @PathVariable Integer serieskey) {
+    @GetMapping("/views/{studydate}/{studykey}/{pid}/{modality}/{serieskey}")
+    public String reportsPatient(@PathVariable String studydate, @PathVariable Integer studykey, @PathVariable String pid, @PathVariable String modality ,@PathVariable Integer serieskey) {
 
         return "reports/viewer";
     }
