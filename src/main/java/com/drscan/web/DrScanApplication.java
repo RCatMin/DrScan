ede301de96bb89ff6e309ad5237a367b0bcfdaef
gusrbs-0404 <gusrbs7534@gmail.com>
2025-03-12 10:23:56 +0900

    Update 학원 subnet 안에서 run

--- src/main/java/com/drscan/web/DrScanApplication.java
+++ src/main/java/com/drscan/web/DrScanApplication.java
@@ -3,14 +3,22 @@ package com.drscan.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class DrScanApplication {
+public class DrScanApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DrScanApplication.class, args);
     }
 
+
+    // 이 작업이 없어도 알어서 잡아준다.
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DrScanApplication.class);
+    }
 }
